d216ef5dd0f87721d410411fe0b511befb9c02ff
Alexander derck Horlings <horlingsalexander@gmail.com>
2023-05-24 22:20:24 +0200

    added umbrella header

--- include/parser/parse_utils/parse_utils.h
+++ include/parser/parse_utils/parse_utils.h
@@ -1,31 +1,21 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        ::::::::            */
-/*   ft_atoi.h                                          :+:    :+:            */
+/*   parse_utils.h                                      :+:    :+:            */
 /*                                                     +:+                    */
 /*   By: ahorling <ahorling@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
-/*   Created: 2023/05/19 21:52:07 by ahorling      #+#    #+#                 */
-/*   Updated: 2023/05/24 22:19:58 by ahorling      ########   odam.nl         */
+/*   Created: 2023/05/24 22:15:05 by ahorling      #+#    #+#                 */
+/*   Updated: 2023/05/24 22:16:23 by ahorling      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef FT_ATOI_H
-# define FT_ATOI_H
+#ifndef PARSE_UTILS_H
+# define PARSE_UTILS_H
 
-# include "ft_atoi.h"
-# include "ft_instring.h"
-# include "ft_memchr.h"
-# include "ft_split.h"
-# include "ft_strchr.h"
-# include "ft_strcmp.h"
-# include "ft_strdup.h"
-# include "ft_strjoin.h"
-# include "ft_strlen.h"
-# include "ft_strncmp.h"
-# include "ft_strrchr.h"
-# include "ft_substr.h"
-# include "get_next_line.h"
-# include "get_next_line_utils.h"
+# include <stdbool.h>
+# include "info.h"
+
+
 
 #endif
